97aad02c78b0c8807098be069b7014768cd159cf
janeth <jarri023@fiu.edu>
2020-09-25 23:01:31 -0400

    create account and sign in

---
+++ src/main/java/rethinq/controllers/StudentController.java
@@ -0,0 +1,49 @@
+package rethinq.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import rethinq.models.Student;
+import rethinq.models.types.StudentLogin;
+import rethinq.repositories.StudentRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin
+@RequestMapping("api/users")
+@RestController
+public class StudentController {
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    @Autowired
+    public StudentRepository studentRepository;
+
+    @PostMapping(path = "/create")
+    public Student createUser(@RequestBody Student student){
+        String encryptedPassword = passwordEncoder.encode(student.getPassword());
+        student.setPassword(encryptedPassword);
+        return studentRepository.save(student);
+    }
+
+    @PostMapping("/login")
+    public Student login(@RequestBody StudentLogin login){
+        Optional<Student> student = studentRepository.findByUsername(login.getUsername());
+        if(!student.isPresent()){
+            throw new Error("Username does not exist");
+        }
+        if(passwordEncoder.matches(login.getPassword(), student.get().getPassword())){
+            return student.get();
+        } else {
+            throw new Error("Password is incorrect!");
+        }
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return studentRepository.findAll();
+    }
+
+
+
+}
--- src/main/java/rethinq/repositories/StudentRepository.java
+++ src/main/java/rethinq/repositories/StudentRepository.java
@@ -3,5 +3,8 @@ package rethinq.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import rethinq.models.Student;
 
+import java.util.Optional;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    public Optional<Student> findByUsername(String username);
 }
